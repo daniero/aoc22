b55a349dc67b11d61f7f924a5a09f5a5a264c23d
Daniel Rødskog <danielmero@gmail.com>
2022-12-11 23:34:09 +0100

    Add ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-function App() {
-
+function App(): JSX.Element {
   return (
     <>
       <h1>Welcome!</h1>
       <p>
-        Here are my solutions to
-        {' '}
+        Here are my solutions to{' '}
         <a href="https://adventofcode.com/2022">Advent of Code 2022</a>!
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
