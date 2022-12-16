4ea813f9aa6563bfcdf4134abaa6c3b3407b34d3
Daniel Rødskog <danielmero@gmail.com>
2022-12-16 22:12:06 +0100

    Solve day 3 part 1

--- src/solutions/03/Day03.tsx
+++ src/solutions/03/Day03.tsx
@@ -1,16 +1,34 @@
 import { ChangeEvent, useState } from 'react';
 import design from '../design.module.css';
 import sampleInput from './sample-input.txt?raw';
-import { solvePart1 } from './part1';
-import { solvePart2 } from './part2';
 
-export default function Day02(): JSX.Element {
+function priority(c: string): number {
+  if (c === c.toLowerCase()) {
+    return c.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+  } else {
+    return c.charCodeAt(0) - 'A'.charCodeAt(0) + 27;
+  }
+}
+
+function solvePart1(input: string): any {
+  return input
+    .split('\n')
+    .flatMap((line) => {
+      const a = [...line.substring(0, line.length / 2)];
+      const b = [...line.substring(line.length / 2)];
+      return [...new Set(a)].filter((c) => b.includes(c));
+    })
+    .map(priority)
+    .reduce((a, b) => a + b, 0);
+}
+
+export default function Day03(): JSX.Element {
   const [input, setInput] = useState(sampleInput);
   const [answer, setAnswer] = useState<number | null>(null);
 
   return (
     <>
-      <h1>Day 2: Rock Paper Scissors</h1>
+      <h1>Day 3: Rucksack Reorganization</h1>
 
       <div className={design.solution}>
         <div>
@@ -27,7 +45,6 @@ export default function Day02(): JSX.Element {
         </div>
         <div>
           <button onClick={() => setAnswer(solvePart1(input))}>Part 1</button>
-          <button onClick={() => setAnswer(solvePart2(input))}>Part 2</button>
           {answer !== null && <pre>{JSON.stringify(answer, undefined, 2)}</pre>}
         </div>
       </div>
